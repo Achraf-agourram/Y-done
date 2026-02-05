d3ff3eb423f3c8b64a26f242ce7c70ef647635b5
Achraf-podcast <achrafagourram6@gmail.com>
2026-02-05 12:42:10 +0100

    added the home to route

---
+++ laravel/app/Http/Controllers/RestaurentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RestaurentController extends Controller
+{
+    public function home ()
+    {
+        return view('home.blade.php');
+    }
+}
